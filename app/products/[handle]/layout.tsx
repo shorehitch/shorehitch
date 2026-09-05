1a625cfe6e58c85d260d55ba10b08a9708726c72
shorehitch <opencanvasaz@gmail.com>
2026-09-05 11:32:16 -0700

    pdp: keep product education inside storefront shell

--- app/products/[handle]/layout.tsx
+++
@@ -1,6 +0,0 @@
-import ProductEducation from "../../../components/storefront/product-education";
-
-export default async function ProductLayout({children,params}:{children:React.ReactNode;params:Promise<{handle:string}>}){
- const {handle}=await params;
- return <>{children}<ProductEducation handle={handle}/></>;
-}
