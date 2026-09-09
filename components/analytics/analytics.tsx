5c6d6e4b228c7655668c4de4e4e2543526de0a00
shorehitch <opencanvasaz@gmail.com>
2026-09-09 11:26:30 -0700

    Use Klaviyo headless onsite loader

--- components/analytics/analytics.tsx
+++ components/analytics/analytics.tsx
@@ -69,12 +69,10 @@ export default function Analytics() {
         `}</Script>
       )}
       {klaviyoKey && (
-        <>
-          <Script id="klaviyo-bootstrap" strategy="beforeInteractive">{`
-            !function(){if(!window.klaviyo){window._klOnsite=window._klOnsite||[];try{window.klaviyo=new Proxy({},{get:function(n,i){return"push"===i?function(){var n;(n=window._klOnsite).push.apply(n,arguments)}:function(){for(var n=arguments.length,o=new Array(n),w=0;w<n;w++)o[w]=arguments[w];var t="function"==typeof o[o.length-1]?o.pop():void 0,e=new Promise(function(n){window._klOnsite.push([i].concat(o,[function(i){t&&t(i),n(i)}]))});return e}}})}catch(n){window.klaviyo=window.klaviyo||{},window.klaviyo.push=function(){var n;(n=window._klOnsite).push.apply(n,arguments)}}}}();
-          `}</Script>
-          <Script src={`https://static.klaviyo.com/onsite/js/${klaviyoKey}/klaviyo.js`} strategy="afterInteractive" />
-        </>
+        <Script
+          src={`https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=${klaviyoKey}`}
+          strategy="afterInteractive"
+        />
       )}
       <Suspense fallback={null}><RouteAnalytics /></Suspense>
     </>
